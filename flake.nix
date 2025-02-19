0dcc278c0e9190e4cbb181659369f771aa413048
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-02-20 00:04:47 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "dashi-image-server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+              sea-orm-cli
+            ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+      }
+    );
+}
